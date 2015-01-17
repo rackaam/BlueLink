3d03dc00690c022481e41b7d3fff059244785926
Matthias <rakam2@free.fr>
2015-01-17 21:27:53 +0100

    Adds BlueLinkOutputStream

---
+++ bluelinklib/src/main/java/eu/rakam/bluelinklib/BlueLinkOutputStream.java
@@ -0,0 +1,90 @@
+package eu.rakam.bluelinklib;
+
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class BlueLinkOutputStream {
+
+    private static final String TAG = "BlueLinkOutputStream";
+
+    private ByteBuffer twoBytesConversionBuffer = ByteBuffer.allocate(2);
+    private ByteBuffer fourBytesConversionBuffer = ByteBuffer.allocate(4);
+    private ByteBuffer eightBytesConversionBuffer = ByteBuffer.allocate(8);
+    private ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    public void writeByte(byte b) {
+        outputStream.write(b);
+    }
+
+    public void writeBoolean(boolean b) {
+        outputStream.write(b ? 1 : 0);
+    }
+
+    public void writeChar(char c) {
+        try {
+            outputStream.write(twoBytesConversionBuffer.putChar(0, c).array());
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    public void writeShort(short s) {
+        try {
+            outputStream.write(twoBytesConversionBuffer.putShort(0, s).array());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeInt(int i) {
+        try {
+            outputStream.write(fourBytesConversionBuffer.putInt(0, i).array());
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    public void writeFloat(float f) {
+        try {
+            outputStream.write(fourBytesConversionBuffer.putFloat(0, f).array());
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    public void writeLong(long l) {
+        try {
+            outputStream.write(eightBytesConversionBuffer.putLong(0, l).array());
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    public void writeDouble(double d) {
+        try {
+            outputStream.write(eightBytesConversionBuffer.putDouble(0, d).array());
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    public void writeString(String s) {
+        if (s == null)
+            return;
+        // Write a char before the string to indicate the string length
+        byte[] stringLengthTab = twoBytesConversionBuffer.putChar(0, (char) s.length()).array();
+        try {
+            outputStream.write(stringLengthTab);
+            outputStream.write(s.getBytes());
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    public byte[] toByteArray() {
+        return outputStream.toByteArray();
+    }
+}
